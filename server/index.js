c80814cc0647a6feac891ec077a964a52800467c
Beverly Hernandez <you@example.com>
2019-09-23 08:42:00 -0400

    base server file

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const parser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+const headers = {
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type'
+}
+
+app.use(morgan('dev'))
+app.use(parser.json())
+app.use(cors(headers))
+
+app.use(express.static(__dirname + '/../client/dist'))
+
+let PORT = process.env.PORT || 8080
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('ERROR connecting to server:', err)
+  }
+  console.log(`artfolio is listening on port ${PORT}!`)
+})
